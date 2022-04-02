7b5d38915ef235d05e3e96a27e8b4d5c81d7253a
MaddoScientisto <maddo.science@gmail.com>
2022-04-02 11:32:11 +0200

    Add project files.

---
+++ MauiApp1Blazor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1Blazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiApp1Blazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1Blazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
